0cacc298d11efa99e23cd70f1eb180ec49662c27
iankaranja420-project <iankaranja420@gmail.com>
2024-10-22 18:24:57 +0300

    Updated Security

--- src/main/java/com/tiba/tiba/Services/UserService.java
+++ src/main/java/com/tiba/tiba/Services/UserService.java
@@ -2,10 +2,14 @@ package com.tiba.tiba.Services;
 
 //import org.springframework.security.crypto.password.PasswordEncoder;
 import com.tiba.tiba.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     private final UserRepository userRepository;
    // private final PasswordEncoder passwordEncoder;
@@ -19,7 +23,8 @@ public class UserService {
     public com.tiba.tiba.Entities.User sign_upNewUser(com.tiba.tiba.Entities.User user) {
 
         // Hash the password before saving the user
-        user.setPassword(user.getPassword());
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
