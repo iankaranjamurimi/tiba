aff461fc52b9790c33dc9e35a55cf8d62df8bf48
iankaranja420-project <iankaranja420@gmail.com>
2024-11-11 14:36:24 +0300

    mondayz

--- src/main/java/com/tiba/tiba/Controllers/MedicalRecordsController.java
+++ src/main/java/com/tiba/tiba/Controllers/MedicalRecordsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/open")
 @RequiredArgsConstructor
 public class MedicalRecordsController {
     private final MedicalRecordsService medicalRecordsService;
