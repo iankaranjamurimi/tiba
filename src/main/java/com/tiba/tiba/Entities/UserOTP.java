f0acf697edeeb26352273da511dd7b484552b556
iankaranja420-project <iankaranja420@gmail.com>
2024-11-25 16:45:50 +0300

    nnn

--- src/main/java/com/tiba/tiba/Entities/UserOTP.java
+++ src/main/java/com/tiba/tiba/Entities/UserOTP.java
@@ -1,28 +1,28 @@
-//package com.tiba.tiba.Entities;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@Getter
-//@Setter
-//@RequiredArgsConstructor
-//public class UserOTP {
-//    @Id
-//    @GeneratedValue
-//    private Long id;
-//
-//    private String otp;
-//
-//    private LocalDateTime expiryTime;
-//
-//    private boolean isUsed;
-//
-//    private boolean isVerified;
-//
-//    @OneToOne
-//    private User user;
-//
-//}
+package com.tiba.tiba.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class UserOTP {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String otp;
+
+    private LocalDateTime expiryTime;
+
+    private boolean isUsed;
+
+    private boolean isVerified;
+
+    @OneToOne
+    private User user;
+
+}
--- src/main/java/com/tiba/tiba/Repositories/OTPRepository.java
+++ src/main/java/com/tiba/tiba/Repositories/OTPRepository.java
@@ -1,13 +1,13 @@
-//package com.tiba.tiba.Repositories;
-//
-//import com.tiba.tiba.Entities.UserOTP;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//@Repository
-//public interface OTPRepository extends JpaRepository<UserOTP, Long> {
-//    void deleteByUserEmail(String email);
-//    Optional<UserOTP> findByUserEmailAndOtpAndIsUsedFalse(String email, String otp);
-//}
+package com.tiba.tiba.Repositories;
+
+import com.tiba.tiba.Entities.UserOTP;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OTPRepository extends JpaRepository<UserOTP, Long> {
+    void deleteByUserEmail(String email);
+    Optional<UserOTP> findByUserEmailAndOtpAndIsUsedFalse(String email, String otp);
+}
